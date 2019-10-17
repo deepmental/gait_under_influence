3ac934961257fad014bba84338ae0049e2cd437d
Emmanuel <emmanuelnsanga@gmail.com>
2019-10-17 15:35:43 +1100

    data collection version 1

--- data_collection/flutter3_app/lib/main.dart
+++ data_collection/flutter3_app/lib/main.dart
@@ -2,97 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sensors/sensors.dart';
 import 'dart:io';
-import 'dart:async';
+import 'dart:async' show future;
 import 'dart:core';
 
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
 
 
-void main() => runApp(MainClass());
+void main() => runApp(DataCollection());
 
-class MainClass extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new TermsPrivacy(),
-      routes: <String, WidgetBuilder>{
-        '/datacollection': (BuildContext context) => new DataCollection(),
-      },
-    );
-  }
-}
-
-
-
-
-class CounterStorage {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-
-      // Read the file
-      String contents = await file.readAsString();
-
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
-  }
-}
-
-
-
-
-class TermsPrivacy extends StatelessWidget {
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Text overflow',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Terms of conditions and privacy policy agreement"),
-        ),
-        body: Column(
-            children: <Widget> [
-
-        Row(
-        children: <Widget>[
-        Expanded( // Constrains AutoSizeText to the width of the Row
-        child: AutoSizeText(
-        agreement,
-          maxLines: 100,
-        )
-      ),
-      ],
-    ),
-              new RaisedButton(
-                  child: const Text('Agree'),
-                  onPressed: () => Navigator.of(context).pushNamed('/datacollection')
-              )
-            ],
-        ),
-      ),
-    );
-  }
-}
 
 
 class DataCollection extends StatelessWidget {
@@ -134,22 +57,24 @@ class DataCollection extends StatelessWidget {
                   onPressed: () async {
 
                     int time = DateTime.now().millisecondsSinceEpoch;
+
+                    DateTime datetime = DateTime.now();
+
                     int count = 0;
                     List IMU  = [];
-                      List gyro = [];
+                    List gyro = [];
 
-                      Stopwatch timer = Stopwatch()..start();
+                    Stopwatch timer = Stopwatch()..start();
 
 
                     gyroscopeEvents.listen((GyroscopeEvent event){
                       gyro.add([event.x, event.y, event.z]);
 
-
                     });
 
                     userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-                      firestore(titleControler.text, bodyControler.text, event.x, event.y, event.z, gyro[0][0], gyro[0][1], gyro[0][2], time);
-                      if (timer.elapsedMilliseconds/1000 >= 4*3600){
+                      firestore(titleControler.text, bodyControler.text, event.x, event.y, event.z, gyro[0][0], gyro[0][1], gyro[0][2], time, datetime.toString());
+                      if (timer.elapsedMilliseconds/1000 >= (4*3600)){
                         exit(0);
 
 
@@ -171,14 +96,22 @@ class DataCollection extends StatelessWidget {
 }
 
 
-firestore(String name, String intoxication, double x, double y, double z, double x1, double y1, double z1, int timestamp) {
+firestore(String name, String intoxication, double x, double y, double z, double x1, double y1, double z1, int timestamp, String datetime) {
 // Delete the database
 // open the database
   DocumentReference document = Firestore.instance.collection('data').document();
-  document.setData({ 'name': name, 'intoxication': intoxication, 'x': x, 'y': y, 'z': z, 'x1': x1, 'y1': y1, 'z1': z1, 'timestamp': timestamp});
-
-
+  document.setData({
+    'name': name,
+    'intoxication': intoxication,
+    'x': x,
+    'y': y,
+    'z': z,
+    'x1': x1,
+    'y1': y1,
+    'z1': z1,
+    'timestamp': timestamp,
+    'datetime': datetime
+  });
 }
-
 
 
